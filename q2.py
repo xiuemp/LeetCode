50c86fdaf00a43c68ae1c6f3122d9df1701cd895
Chris.Liu <liuqlxyz@163.com>
2015-08-18 01:00:19 +0800

    initial q3

--- q2.py
+++ q2.py
@@ -1,4 +1,10 @@
 ##补齐以简化成统一循环；两数来保存前后进位
+"""
+You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+"""
 
 class ListNode:
     def __init__(self, x):
