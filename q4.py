2859f7962eb7c7082bb3a2105694cc70d6807894
Chris.Liu <liuqlxyz@163.com>
2015-08-19 03:39:42 +0800

    add q4

---
+++ q4.py
@@ -0,0 +1,10 @@
+#
+"""
+There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+"""
+class Solution:
+    # @param {integer[]} nums1
+    # @param {integer[]} nums2
+    # @return {float}
+    def findMedianSortedArrays(self, nums1, nums2):
+        
